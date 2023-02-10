8854b48cedf5e5df6c2b5cf648329104503d731c
Harley De Jesus Taberner Garcia <hjtaberner@personalsoft.com.co>
2023-02-10 11:03:40 -0500

    se crea proyecto y configuracion bd

---
+++ src/main/java/com/polizas/pruebaApiPersonalSoft/PruebaApiPersonalSoftApplication.java
@@ -0,0 +1,13 @@
+package com.polizas.pruebaApiPersonalSoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaApiPersonalSoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaApiPersonalSoftApplication.class, args);
+	}
+
+}
