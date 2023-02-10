b18f32a2680471dee15513eda0efd5452bef9aad
Harley De Jesus Taberner Garcia <hjtaberner@personalsoft.com.co>
2023-02-10 11:44:39 -0500

    se añaden las variables de la clase model

--- src/main/java/com/polizas/pruebaApiPersonalSoft/model/Poliza.java
+++ src/main/java/com/polizas/pruebaApiPersonalSoft/model/Poliza.java
@@ -1,4 +1,26 @@
 package com.polizas.pruebaApiPersonalSoft.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.sql.Date;
+
+//significa que es una entidad de base de datos
+@Entity
 public class Poliza {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idPoliza;
+    private String nombre;
+    private Integer idCliente;
+    private Date fechaNacimiento;
+    private Date fechaPoliza;
+    private String coberturaPoliza;
+    private String nombrePlanPoliza;
+    private String ciudadResidencia;
+    private String direccionResidencia;
+    private int modelo;
+    private String placa;
 }
